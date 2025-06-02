46ccf0467b565c00501447d383755609a10bda33
juliocesardemoraes <jcmcf2012@gmail.com>
2025-06-02 18:09:28 -0300

    feat: add props class

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-import Card from "./Card";
+import Student from "./Student";
+
+const createStudent = (name, age, email, course) => {
+  return name;
+};
+
+createStudent(name);
 
 function App() {
   return (
     <>
       <h1>Ola</h1>
-      <Card></Card>
+      <Student
+        name={"Bruno"}
+        age={22}
+        email={"bruno@gmail.com"}
+        course="Agro"
+      ></Student>
+      <Student
+        name={"Natalia"}
+        age={21}
+        email={"natalia@gmail.com"}
+        course="Sistemas de informação"
+      ></Student>
     </>
   );
 }
