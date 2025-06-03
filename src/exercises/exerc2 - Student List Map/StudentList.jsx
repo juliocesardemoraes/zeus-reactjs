a6d4edf6fec0b03d2be63220953752ea85b3f31d
juliocesardemoraes <jcmcf2012@gmail.com>
2025-06-02 21:03:47 -0300

    fix: remove code

--- src/exercises/exerc2 - Student List Map/StudentList.jsx
+++ src/exercises/exerc2 - Student List Map/StudentList.jsx
@@ -1,12 +1,5 @@
 import React from "react";
-import { studentsMock } from "./studentsMock";
 
 export default function StudentList() {
-  return (
-    <ul>
-      {studentsMock.map((user) => {
-        return <li key={user.id}>{user.nome}</li>;
-      })}
-    </ul>
-  );
+  return <ul></ul>;
 }
