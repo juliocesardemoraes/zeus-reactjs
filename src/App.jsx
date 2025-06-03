6ba5f2619449221fd274d42d64a3d702917fd44a
juliocesardemoraes <jcmcf2012@gmail.com>
2025-06-03 11:24:14 -0300

    feat: initial code

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,7 @@
-import FlexContainer from "./FlexContainer";
-import { studentsMock } from "./mockStudents";
-import Student from "./Student";
-
 function App() {
   return (
     <>
-      <h1>Ola</h1>
-      <FlexContainer>
-        {studentsMock.map((student) => {
-          return (
-            <Student
-              key={student.id}
-              name={student.nome}
-              age={student.idade}
-              email={student.email}
-              course={student.curso}
-            ></Student>
-          );
-        })}
-      </FlexContainer>
-      {/* <Student
-        name={"Bruno"}
-        age={22}
-        email={"bruno@gmail.com"}
-        course="Agro"
-      ></Student>
-      <Student
-        name={"Natalia"}
-        age={21}
-        email={"natalia@gmail.com"}
-        course="Sistemas de informação"
-      ></Student> */}
+      <h1>Ciclo de vida</h1>
     </>
   );
 }
--- src/FlexContainer.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function FlexContainer({ children }) {
-  return <div className="flex">{children}</div>;
-}
