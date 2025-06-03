eb2863dabbedbc6772ca77d283e0eb578762f50c
juliocesardemoraes <jcmcf2012@gmail.com>
2025-06-03 15:09:37 -0300

    feat: add project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import { tasksMock } from "./mock/TasksMock";
 
 function App() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+  const [tasks, setTasks] = useState(tasksMock);
 
   useEffect(() => {
     console.log("TITLE", title);
@@ -12,29 +14,54 @@ function App() {
   return (
     <>
       <h1>Criar Tarefa</h1>
-      <input
-        type="text"
-        name="title"
-        placeholder="Digite um título"
-        id="title"
-        value={title}
-        onChange={(event) => {
-          setTitle(event.target.value);
-        }}
-      />
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
 
-      <textarea
-        name="description"
-        placeholder="Digite uma descrição"
-        id=""
-        onChange={(event) => {
-          setDescription(event.target.value);
+          setTasks((prev) => {
+            return [
+              ...prev,
+              {
+                title,
+                description,
+                done: false,
+              },
+            ];
+          });
         }}
-      ></textarea>
+      >
+        <input
+          type="text"
+          name="title"
+          placeholder="Digite um título"
+          id="title"
+          value={title}
+          onChange={(event) => {
+            setTitle(event.target.value);
+          }}
+        />
+
+        <textarea
+          name="description"
+          placeholder="Digite uma descrição"
+          id=""
+          onChange={(event) => {
+            setDescription(event.target.value);
+          }}
+        ></textarea>
+
+        <button type="submit">Criar Tarefa</button>
+      </form>
 
       <h1>Lista de Tarefas</h1>
-      <h2>{title}</h2>
-      <h3>{description}</h3>
+      {tasks.map((task, index) => {
+        return (
+          <div key={index}>
+            <h2>{task.title}</h2>
+            <h3>{task.description}</h3>
+          </div>
+        );
+      })}
     </>
   );
 }
---
+++ src/mock/TasksMock.js
@@ -0,0 +1,17 @@
+export const tasksMock = [
+  {
+    title: "Estudar React",
+    description: "Focar nos hooks useState e useEffect",
+    done: false,
+  },
+  {
+    title: "Criar portfólio",
+    description: "Adicionar seção de projetos e contato",
+    done: false,
+  },
+  {
+    title: "Organizar arquivos",
+    description: "Deletar projetos antigos e organizar por pasta",
+    done: false,
+  },
+];
