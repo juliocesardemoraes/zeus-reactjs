a16a2bca78b9f90c929f280e0c4a1fee74643280
juliocesardemoraes <jcmcf2012@gmail.com>
2025-06-03 16:32:45 -0300

    feat: add api class

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,56 @@
-import "./globals.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
-  return <></>;
+  const [data, setData] = useState([]);
+  const [resource, setResource] = useState("posts");
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    (async () => {
+      const response = await axios.get(
+        `https://jsonplaceholder.typicode.com/${resource}`
+      );
+      setData(response.data);
+      setLoading(false);
+    })();
+  }, [resource]);
+
+  return (
+    <>
+      <button
+        onClick={() => {
+          setResource("users");
+        }}
+      >
+        users
+      </button>
+      <button
+        onClick={() => {
+          setResource("posts");
+        }}
+      >
+        posts
+      </button>
+      <button
+        onClick={() => {
+          setResource("comments");
+        }}
+      >
+        comments
+      </button>
+
+      {loading && <>Carregando</>}
+
+      <ul>
+        {data.map((item, index) => {
+          return <li key={index}>{JSON.stringify(item)}</li>;
+        })}
+      </ul>
+    </>
+  );
 }
 
 export default App;
